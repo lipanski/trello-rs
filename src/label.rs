3e6ba84ec4088920dba762e165bec0f6d9579e8e
lipanski <florin.lipan@blacklane.com>
2015-12-27 13:04:39 +0100

    List labels

---
+++ src/label.rs
@@ -0,0 +1,20 @@
+use Client;
+use Error;
+
+#[derive(RustcDecodable, RustcEncodable)]
+#[allow(non_snake_case)]
+pub struct Label {
+    id: String,
+    idBoard: String,
+    name: String,
+    color: String,
+    uses: u32
+}
+
+impl Label {
+    pub fn list(client: &Client, board_id: &str) -> Result<Vec<Self>, Error> {
+        let path = ["boards", board_id, "labels"].join("/");
+
+        client.get(&path)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,13 @@ pub mod client;
 pub mod board;
 pub mod list;
 pub mod card;
+pub mod label;
 
 pub type Client = client::Client;
 pub type Board = board::Board;
 pub type List = list::List;
 pub type Card = card::Card;
+pub type Label = label::Label;
 
 pub enum Error {
     NotFound,
